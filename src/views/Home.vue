f4d04903eb0bcea58559244214e2667a01d98d7d
rotry <1355154383@qq.com>
2022-07-24 11:28:34 +0800

    将轮播选项卡注册到首页中

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,66 @@
 <template>
-	<div class="home">主页</div>
+	<div class="home">
+		<div class="home_swiper">
+			<Swiper :catagoryList="catagoryList"></Swiper>
+		</div>
+		<div>
+			<ul>
+				<li>
+
+				</li>
+			</ul>
+		</div>
+	</div>
 </template>
 
 <script>
+import fetch from "../utils/fetch";
+import Swiper from "../components/Swiper";
+
 export default {
-	name: "Home"
+	name: "Home",
+	data() {
+		return {
+			address: "",
+			catagoryList: [],
+			shopList:[]
+		}
+	},
+	components: {
+		Swiper
+	},
+	mounted() {
+		this.getAddressBygeohash();
+		this.getCatagoryList();
+		this.getShop();
+	},
+	methods: {
+		getAddressBygeohash() {
+			fetch(`/v2/pois/${localStorage.getItem('geohash')}`,
+				{}, 'GET').then(res => {
+				this.address = res;
+			})
+		},
+		getCatagoryList() {
+			fetch('/v2/index_entry', {}, 'GET').then(res => {
+				this.catagoryList = res;
+			})
+		},
+		// 获取店铺
+		getShop() {
+			// 获取经纬度
+			let geohash = localStorage.getItem('geohash');
+			let array = geohash.split(',');
+			fetch('/shopping/restaurants', {
+
+				latitude: array[0],
+				longitude: array[1]
+
+			}, 'GET').then(res => {
+				console.log(res)
+			})
+		}
+	}
 }
 </script>
 
