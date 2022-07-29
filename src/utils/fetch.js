8aa4d8cd5f247932043e12eff3d3787da694094c
rotry <1355154383@qq.com>
2022-07-29 11:48:18 +0800

    重新封装了fetch

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,78 +1,57 @@
-const baseUrl = 'https://elm.cangdu.org';
 
-
-export default async(url = '', data = {}, type = 'GET', method = 'fetch') => {
+/**
+ * 封装fetch的请求
+ * @param url 请求的地址
+ * @param data 请求的参数
+ * @param type 请求方式，目前只针对get和post
+ */
+const baseUrl = 'https://elm.cangdu.org'
+export default async (url,data={},type) => {
+	// 大写请求
 	type = type.toUpperCase();
+	// 拼接地址
 	url = baseUrl + url;
+	// get请求
+	if (type === 'GET'){
+		// 判断师父有参数
+		let string = "";
+		if (data){
+			string = new URLSearchParams(data).toString();
+		}
+		if (string){
+			url = `${url}?${string}`;
+		}
+		// 发送请求
+		return new Promise(async (resolve, reject) => {
+			try{
+				let response = await fetch(url, {
+					method: type
+				});
+				resolve(response.json());
+			}catch (error){
+				reject(error);
+			}
 
-	if (type === 'GET') {
-		let dataStr = ''; //数据拼接字符串
-		Object.keys(data).forEach(key => {
-			dataStr += key + '=' + data[key] + '&';
 		})
 
-		if (dataStr !== '') {
-			dataStr = dataStr.substr(0, dataStr.lastIndexOf('&'));
-			url = url + '?' + dataStr;
-		}
 	}
+	if (type==='POST'){
 
-	if (window.fetch && method === 'fetch') {
-		let requestConfig = {
-			credentials: 'include',
-			method: type,
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			},
-			mode: "cors",
-			cache: "force-cache"
-		}
-
-		if (type === 'POST') {
-			Object.defineProperty(requestConfig, 'body', {
-				value: JSON.stringify(data)
-			})
-		}
-
-		try {
-			const response = await fetch(url, requestConfig);
-			const responseJson = await response.json();
-			return responseJson
-		} catch (error) {
-			throw new Error(error)
-		}
-	} else {
-		return new Promise((resolve, reject) => {
-			let requestObj;
-			if (window.XMLHttpRequest) {
-				requestObj = new XMLHttpRequest();
-			} else {
-				requestObj = new ActiveXObject;
-			}
-
-			let sendData = '';
-			if (type === 'POST') {
-				sendData = JSON.stringify(data);
-			}
-
-			requestObj.open(type, url, true);
-			requestObj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-			requestObj.send(sendData);
-
-			requestObj.onreadystatechange = () => {
-				if (requestObj.readyState === 4) {
-					if (requestObj.status === 200) {
-						let obj = requestObj.response
-						if (typeof obj !== 'object') {
-							obj = JSON.parse(obj);
-						}
-						resolve(obj)
-					} else {
-						reject(requestObj)
-					}
-				}
+		return new Promise(async (resolve, reject) => {
+			// 发送请求
+			try {
+				let response = await fetch(url,{
+					method:type,
+					headers: {
+						"Content-Type":"application/json"
+					},
+					body:JSON.stringify(data)
+				});
+				resolve(response.json());
+			}catch (error){
+				reject(error);
 			}
 		})
+
 	}
 }
